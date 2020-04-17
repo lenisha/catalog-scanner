6a41b2646fb3d49677cfc8400af6503f8a0aaf0b
Elena Neroslavskaya <lenisha@gmail.com>
2020-04-17 16:32:27 -0400

    initial frame

---
+++ DataCatalogResultsScanner.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using DataCatalogGen2;
+using System.Net.Http;
+using DataCatalogGen2.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Rest;
+
+namespace catalog_scanner
+{
+    class DataCatalogResultsScanner
+    {
+        private static string BASE_URI = "https://contoso.catalog.azure.com/api/atlas";
+
+        ILogger _Logger;
+        IConfiguration _Config;
+
+
+        public DataCatalogResultsScanner(ILoggerFactory logger, IConfiguration config)
+        {
+
+            _Logger = logger.CreateLogger<DataCatalogResultsScanner>();
+            _Config = config;
+        }
+
+        public void Run()
+        {
+            // Get token and set auth
+            var svcClientCreds = new TokenCredentials(getToken(), "Bearer");
+            var client = new DataCatalogClient(svcClientCreds);
+            // Overwrite base URI if needed
+            client.BaseUri = new System.Uri(BASE_URI);
+            // /v2/entity/guid/{guid}
+            var task = client.EntityREST.GetByIdWithHttpMessagesAsync("97c36ab1-016f-f099-63d1-042f71e6de81");
+            task.Wait();
+
+            _Logger.LogInformation("\nURI:\n {0}", task.Result.Request.RequestUri);
+            _Logger.LogInformation("\nStatus Code:\n {0}", task.Result.Response.StatusCode);
+            printResultBody(task.Result.Body);
+
+        }
+
+        private  void printResultBody(Object body)
+        {
+            _Logger.LogInformation("\n Result:\n {0}", JsonConvert.SerializeObject(body, Formatting.Indented));
+        }
+
+        private  string getToken()
+        {
+            string clientId = _Config.GetValue<String>("ClientId");
+            string clientSecret = _Config.GetValue<String>("ClientSecret");
+            string tenantId = _Config.GetValue<String>("TenantId");
+
+            string loginUrl = String.Format("https://login.windows.net/{0}/oauth2/token", tenantId);
+
+            var values = new Dictionary<string, string>
+                     {
+                     { "grant_type", "client_credentials" },
+                     { "client_id", clientId },
+                     { "client_secret", clientSecret },
+                     { "resource", "https://management.core.windows.net/" }
+                     };
+
+            var content = new FormUrlEncodedContent(values);
+            HttpClient authClient = new HttpClient();
+            var bearerResult = authClient.PostAsync(loginUrl, content);
+            bearerResult.Wait();
+            var resultContent = bearerResult.Result.Content.ReadAsStringAsync();
+            resultContent.Wait();
+            var bearerToken =
+            JObject.Parse(resultContent.Result)["access_token"].ToString();
+
+            return bearerToken;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.Extensions.Logging;
+
+namespace catalog_scanner
+{
+    class Program
+    {
+          static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("ADC Gen 2 Client");
+
+            // create service collection
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            // create service provider
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // run app
+            serviceProvider.GetService <DataCatalogResultsScanner>().Run();
+
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+
+            string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            if (String.IsNullOrEmpty(env))
+                env = "Production";
+
+            // Set up configuration sources.
+            var configuration = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
+              .AddEnvironmentVariables()
+              .Build();
+
+            // add logging
+            serviceCollection.AddLogging(builder =>
+            {
+                builder.AddConsole();
+                builder.AddDebug();
+            });
+
+            // Add Config
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+            serviceCollection.AddOptions();
+            // add app
+            serviceCollection.AddTransient <DataCatalogResultsScanner>();
+            Console.WriteLine("\nDataCatalogResultsScanner Loaded.");
+        }
+    }
+}
